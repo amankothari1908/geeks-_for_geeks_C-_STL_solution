69ed920f2e4fa19a7b46eff455de41577b4ee4f4
freakycoder00 <60997361+freakycoder00@users.noreply.github.com>
2020-09-30 09:29:28 +0530

    Update Make pair.cpp

--- Pairs/Make pair.cpp
+++ Pairs/Make pair.cpp
@@ -1,27 +1,27 @@
-Question::
+**Question::**
 Given two elements X and Y. The task is to return the pair formed with given elements
-Input Format: The input contains T, number of testcases. Each testcase contains one line of input containing X and Y separated by space.
-Output Format: For each testcase, in a new line, you need to print the elements of pair formed.
-User Task: You need to complete the function makePair(X, Y) that takes two numbers as parameters and returns a pair of these number. 
+**Input Format:** The input contains T, number of testcases. Each testcase contains one line of input containing X and Y separated by space.
+**Output Format:** For each testcase, in a new line, you need to print the elements of pair formed.
+**User Task:** You need to complete the function makePair(X, Y) that takes two numbers as parameters and returns a pair of these number. 
            The first number is the first member of pair and second number is the second member. The printing is done by the driver code.
 
-Constraints:
+**Constraints:**
 1 <= T <= 100
 1 <= X, Y <= 1000
 
-Sample Input:
+**Sample Input:**
 2
 4 5
 1 0
 
-Sample Output:
+**Sample Output:**
 (4,5)
 (1,0)
 
-Explanation:
+**Explanation:**
 Testcase1: We have the numbers 4 and 5, so we make them a pair.
 
-code::
+**code::**
 //Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
